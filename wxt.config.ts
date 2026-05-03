1009bf8a7e8fe9e75dc242e4e88aea0588324e7e
RonkTsang <ronktsang@gmail.com>
2026-05-03 19:11:50 +0800

    chore: lower firefox minimum version

--- wxt.config.ts
+++ wxt.config.ts
@@ -59,7 +59,7 @@ export default defineConfig({
       manifest.browser_specific_settings = {
         gecko: {
           id: 'gemini-power-kit@ronktsang.com',
-          strict_min_version: '148.0',
+          strict_min_version: '140.0',
           data_collection_permissions: {
             required: ['none'],
             optional: [],
